6af8f45270b4cdc0855697adfbb19d4d33c0fe81
Gareth Rushgrove <gareth@morethanseven.net>
2013-01-27 21:25:41 +0000

    Puppet now contains the pip provider, see notes from puppet-pip github repo

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,10 +1,8 @@
 node default {
-  
   exec { "apt-update":
     command => "/usr/bin/apt-get update",
   }
   Exec["apt-update"] -> Package <| |>
-  include puppet_pip
-  include dora
 
+  include dora
 }
